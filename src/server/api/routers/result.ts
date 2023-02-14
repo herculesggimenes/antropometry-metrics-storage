7e19cb4a2a8b105a2b2546bb9053c454997a4a3f
herculesgimenes <herculesggimenes@gmail.com>
2023-02-14 15:50:16 -0300

    working

---
+++ src/server/api/routers/result.ts
@@ -0,0 +1,65 @@
+import stream from "stream";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const resultsRouter = createTRPCRouter({  
+  getAll: publicProcedure.mutation(({ ctx }) => {
+    return ctx.prisma.result.findMany();
+  }),
+  
+  publish: publicProcedure
+  .input(z.object({
+    gender: z.string(),
+    m_1_1: z.number(),
+    m_1_2: z.number(),
+    m_1_3: z.number(),
+    m_1_4: z.number(),
+    m_1_5: z.number(),
+    m_1_6: z.number(),
+    m_1_7: z.number(),
+    m_1_8: z.number(),
+    m_1_9: z.number(),
+    m_1_10: z.number(),
+    m_2_1: z.number(),
+    m_2_2: z.number(),
+    m_2_3: z.number(),
+    m_2_4: z.number(),
+    m_2_5: z.number(),
+    m_2_6: z.number(),
+    m_2_7: z.number(),
+    m_2_8: z.number(),
+    m_2_9: z.number(),
+    m_2_10: z.number(),
+    m_2_11: z.number(),
+    m_2_12: z.number(),
+    m_2_13: z.number(),
+    m_3_1: z.number(),
+    m_3_2: z.number(),
+    m_3_3: z.number(),
+    m_3_4: z.number(),
+    m_3_5: z.number(),
+    m_4_1: z.number(),
+    m_4_2: z.number(),
+    m_4_3: z.number(),
+    m_4_4: z.number(),
+    m_4_5: z.number(),
+    m_4_6: z.number(),
+    m_4_7: z.number(),
+    m_5_1: z.number(),
+    m_5_2: z.number(),
+    m_5_3: z.number(),
+  }))
+  .mutation(({input, ctx}) => {
+    const response = ctx.prisma.result.create({
+      data: input
+    })
+    console.log(response)
+    return {
+      result: "Success"
+    }
+  })
+  
+});
+
+
+
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -18,13 +18,8 @@
  */
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { type Session } from "next-auth";
-
-import { getServerAuthSession } from "../auth";
 import { prisma } from "../db";
 
-type CreateContextOptions = {
-  session: Session | null;
-};
 
 /**
  * This helper generates the "internals" for a tRPC context. If you need to use
@@ -35,9 +30,8 @@ type CreateContextOptions = {
  * - trpc's `createSSGHelpers` where we don't have req/res
  * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
  */
-const createInnerTRPCContext = (opts: CreateContextOptions) => {
+const createInnerTRPCContext = () => {
   return {
-    session: opts.session,
     prisma,
   };
 };
@@ -51,11 +45,8 @@ export const createTRPCContext = async (opts: CreateNextContextOptions) => {
   const { req, res } = opts;
 
   // Get the session from the server using the unstable_getServerSession wrapper function
-  const session = await getServerAuthSession({ req, res });
 
-  return createInnerTRPCContext({
-    session,
-  });
+  return createInnerTRPCContext();
 };
 
 /**
@@ -96,29 +87,3 @@ export const createTRPCRouter = t.router;
  */
 export const publicProcedure = t.procedure;
 
-/**
- * Reusable middleware that enforces users are logged in before running the
- * procedure
- */
-const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session || !ctx.session.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-  return next({
-    ctx: {
-      // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
-    },
-  });
-});
-
-/**
- * Protected (authed) procedure
- *
- * If you want a query or mutation to ONLY be accessible to logged in users, use
- * this. It verifies the session is valid and guarantees ctx.session.user is not
- * null
- *
- * @see https://trpc.io/docs/procedures
- */
-export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
